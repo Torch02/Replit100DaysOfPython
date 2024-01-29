74212e80668b90e2b145b9374a656abe1868e76a
Torch02 <torch02@gmail.com>
2024-01-29 07:02:05 -0500

    Day 48 complete

--- Day048/Day048.py
+++ Day048/Day048.py
@@ -1,1 +1,20 @@
+import os, time
+f = open("high.score", "a+")
 
+while True:
+  os.system("clear")
+  initials = input("Player initials: ")
+  score = int(input("Score: "))
+
+  f.write(f"{initials} {score}\n")
+
+  choice = input("Input more scores? (y/n): ")
+  if choice.lower()[0] == "y":
+    time.sleep(1)
+    continue
+  else:
+    break
+
+f.close()
+
+exit()
