754157c68382cc1cf12eaff0c4fac1e3732e9f4b
Torch02 <torch02@gmail.com>
2024-01-30 07:11:11 -0500

    Day 49 complete

--- Day049/Day049.py
+++ Day049/Day049.py
@@ -1,1 +1,22 @@
+f = open("high.score", "r")
 
+scores = {}
+
+while True:
+  tempLine = f.readline().strip()
+  if tempLine == "":
+    break
+  else:
+    scores[tempLine.split()[0]] = int(tempLine.split()[1])
+
+highName = ""
+highScore = 0
+
+for name in scores.keys():
+  if scores[name] > highScore:
+    highName = name
+    highScore = scores[name]
+
+print(f"The best player is {highName} with a score of {highScore}")
+
+exit()
