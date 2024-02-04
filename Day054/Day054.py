dcb534988c8d6e09ce1283cf03e0aad8ecfc51ad
Torch02 <torch02@gmail.com>
2024-02-04 08:35:29 -0500

    Day 54 complete

--- Day054/Day054.py
+++ Day054/Day054.py
@@ -1,1 +1,12 @@
+import csv # Imports the csv library
 
+with open("Day54Totals.csv") as file: 
+  reader = csv.DictReader(file) # Treats the file as a dictionary 
+  total = 0.0
+  for row in reader: 
+    total += float(row["Cost"])*float(row["Quantity"])
+
+print("🌟Shop $$ Tracker🌟")
+print(f"Your shop took in ${round(total,2)} total.")
+
+exit()
