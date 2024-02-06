b74c496cd1fff78842871065d3e9526979f6c0a2
Torch02 <torch02@gmail.com>
2024-02-06 07:14:00 -0500

    Day 56 complete

--- Day056/Day056.py
+++ Day056/Day056.py
@@ -1,1 +1,17 @@
+import os, csv
 
+with open("100MostStreamedSongs.csv") as file:
+  f = csv.reader(file)
+
+  for row in f:
+    tempArtist = row[3]
+    # create dir if it doesn't exist
+    try:
+      os.mkdir(tempArtist)
+    except:
+      pass
+    # create file in sub-dir
+    tempSong = open(f"{tempArtist}/{row[1]}.txt","w")
+    tempSong.close()
+
+exit()
