b6248ce23bcb93c6375826f3b80b49ae38703e3b
Torch02 <torch02@gmail.com>
2024-02-07 06:54:31 -0500

    Day 57 complete

--- Day057/Day057.py
+++ Day057/Day057.py
@@ -1,1 +1,12 @@
+def factorial(number):
+  if number == 1:
+    return 1
+  else:
+    return number * factorial(number - 1)
 
+print("🌟Factorial Finder🌟")
+number = int(input("Input a number > "))
+fac = factorial(number)
+print(f"The factorial of {number} is {fac}.")
+
+exit()
