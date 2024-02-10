4bc968a41eeb23327019e40ff0d1ee5e1f372781
Torch02 <torch02@gmail.com>
2024-02-10 08:46:17 -0500

    When you mess with time, time messes back...

--- Day060/Day060.py
+++ Day060/Day060.py
@@ -1,1 +1,21 @@
+import datetime
 
+today = datetime.date.today() 
+
+print("🌟Event Countdown Timer🌟")
+
+name = input("Input the event > ")
+year = int(input("Input the year > "))
+month = int(input("Input the month > "))
+day = int(input("Input the day > "))
+holiday = datetime.date(year = year, month = month, day = day) 
+delta = abs((holiday - today).days)
+
+if today < holiday:
+  print(f"{name} is in {delta} days")
+elif today > holiday:
+  print(f"{name} was {delta} days ago")
+else:
+  print(f"{name} is today!!")
+
+exit()
