ebcf57c749a0ad4f971965d1b79633e983540105
Torch02 <torch02@gmail.com>
2024-02-19 05:57:46 -0500

    Protest code

--- Day069/Day069.py
+++ Day069/Day069.py
@@ -1,1 +1,6 @@
+print("PROTEST CODE")
+print("I didn't start #100DaysOfCode to create images that aren't provided")
+print("Did these get deleted at some point?")
+print("The pictures we were supposed to use on Day 68 weren't there either.")
 
+exit()
