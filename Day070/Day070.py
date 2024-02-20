3f591b5c1f5addae04f0f006ba08e08d911db9b3
Torch02 <torch02@gmail.com>
2024-02-20 06:59:29 -0500

    back to regular python

--- Day070/Day070.py
+++ Day070/Day070.py
@@ -1,1 +1,14 @@
+import os
 
+userName = input("Username > ")
+userPass = input("Password > ")
+
+try:
+  if userPass == os.environ[userName]:
+    print(f"Welcome, {userName}")
+  else:
+    print("Bad username or password")
+except:
+  print("Bad username or password")
+
+exit()
