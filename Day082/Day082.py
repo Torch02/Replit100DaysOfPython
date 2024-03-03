8bbd5cf33b66cfb5f65eb92ba5e0e17b7e31aa8a
Torch02 <torch02@gmail.com>
2024-03-03 14:36:27 -0500

    get 'er dun

--- Day082/Day082.py
+++ Day082/Day082.py
@@ -1,1 +1,19 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route('/language', methods=["GET"])
+def language():
+  get = request.args
+  if get == {}:
+    return "No data found"
+  if get["lang"] == "eng":
+    return "Welcome to our webpage."
+  elif get["lang"] == "fr":
+    return "Bonjour! Bienvenue ici."
+
+@app.route('/', methods=["GET"])
+def index():
+  return "No data"
+
+app.run(host='0.0.0.0', port=81)
